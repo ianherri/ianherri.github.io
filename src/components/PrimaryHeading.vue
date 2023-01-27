cf8b3a43414975ebe49728b5db409946ea9fbb2f
Ian Herrington <ianherri20@gmail.com>
2023-01-26 18:01:51 -0600

    add typing ani

--- src/components/PrimaryHeading.vue
+++ src/components/PrimaryHeading.vue
@@ -1,15 +1,36 @@
 <script setup>
-defineProps({
+import { onMounted, ref } from "vue";
+
+const props = defineProps({
   heading: String,
   subHeading: String,
   content: String,
+});
+
+const typedHeading = ref("");
+const blink = ref(false);
+
+onMounted(() => {
+  let index = 0;
+  const typing = setInterval(() => {
+    typedHeading.value += props.heading[index];
+    index++;
+    if (index === props.heading.length) {
+      clearInterval(typing);
+    }
+  }, 200);
+
+  setInterval(() => {
+    blink.value = !blink.value;
+  }, 500);
 });
 </script>
 
 <template>
   <div class="text-white max-w-lg mx-auto px-10">
-    <h1 class="animate-fade-in-one text-5xl font-bold font-serif mt-4">
-      {{ heading }}
+    <h1 class="text-5xl font-bold font-serif mt-4">
+      {{ typedHeading }}
+      <span :class="{ 'typing-bar': true, blink: blink }"></span>
     </h1>
     <h2 class="animate-fade-in-one text-2xl font-extralight mt-4">
       {{ subHeading }}
@@ -20,7 +41,7 @@ defineProps({
   </div>
 </template>
 
-<style>
+<style scoped>
 .animate-fade-in-one {
   animation: fade-in ease-in 1;
   animation-fill-mode: both;
@@ -31,6 +52,23 @@ defineProps({
   animation: fade-in ease-in 1;
   animation-fill-mode: both;
   animation-duration: 2s;
+}
+
+.typing-bar {
+  border-right: 1px solid rgb(255, 255, 255);
+  animation: blink 0.7s infinite;
+  padding-left: 2px;
+}
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 
 @keyframes fade-in {
